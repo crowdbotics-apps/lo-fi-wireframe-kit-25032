0c25363fd425bd2b6ddaa22cee5d7464a9b6a3ff
Crowdbotics <team@crowdbotics.com>
2021-03-15 01:14:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lo_fi_wireframe_kit_25032/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lo_fi_wireframe_kit_25032;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "lo_fi_wireframe_kit_25032";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://lo-fi-wireframe-kit-25032.botics.co",
+  defaultTimeout: 5000
+};
